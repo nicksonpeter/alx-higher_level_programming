e0cb9adc2f714023572acc7cfbebfbe5d29dd786
nicksonpeter <nickrocky5200@gmail.com>
2023-05-18 19:19:47 +0000

    adding file

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,3 @@
 -- updating a record
-UPDATE TABLE second_table SET id = 10 WHERE name = "Bob";
+UPDATE TABLE second_table SET score = 10 WHERE name = "Bob";
 SELECT score, name FROM second_table ORDER BY score DESC;
