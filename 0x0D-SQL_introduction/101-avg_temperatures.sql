2a08542ab06debfb05a169e3c0d00875eb42b553
nicksonpeter <nickrocky5200@gmail.com>
2023-05-19 15:52:03 +0000

    adding file

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,2 @@
+-- displaying average cities
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,2 @@
+-- List max temperature
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state ASC;
