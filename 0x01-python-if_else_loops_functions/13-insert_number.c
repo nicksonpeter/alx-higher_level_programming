7dfa91ae41e2742fdfa223a3e713c6dc5cc237cd
nicksonpeter <nickrocky5200@gmail.com>
2024-01-29 02:20:01 -0800

    updating file

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -4,8 +4,7 @@
  * insert_node - Inserts a number into a sorted singly-linked list.
  * @head: A pointer the head of the linked list.
  * @number: The number to insert.
- * Return: If the function fails - NULL.
- *         Otherwise - a pointer to the new node.
+ * Return: 0 If the function fails or pointer to the new node.
  */
 listint_t *insert_node(listint_t **head, int number)
 {
